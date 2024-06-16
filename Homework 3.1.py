9817fe0c3869f910511e72e7df76014aca531ec0
ViktoriiaPankratova <155125488+ViktoriiaPankratova@users.noreply.github.com>
2024-06-16 19:19:52 +0200

    Initial commit

--- Homework 3.1.py
+++ Homework 3.1.py
@@ -7,8 +7,10 @@ elif user_operation == '-':
     result = user_number1 + user_number2
 elif user_operation == '*':
     result = user_number1 + user_number2
-elif user_operation == '/' and user_number2 !=0:
+if user_number2 != 0:
     result = user_number1 / user_number2
+else:
+    result = 'дільник не може дорівнювати нулю'
 
 print(f"{user_number1} {user_operation} {user_number2} = {result}")
 
